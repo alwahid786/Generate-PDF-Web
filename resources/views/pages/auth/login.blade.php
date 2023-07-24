06f7f8189cd444481c383a5d499aa0c2fd253cbe
ZeeshanAhsan <zeeshan.kodex@gmail.com>
2023-07-24 15:24:54 +0500

    updated

---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.layout-default')
+@section('content')
+<h1>login</h1>
+@endsection
+@section('insertjavascript')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.login');
+    return view('pages.auth.login');
 });
