a66a0eb5a6d61b6baa59c36f136790cf695b186d
Wahid Ahmad <wahid.a@kodextech.com>
2023-07-25 15:02:26 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
 Route::get('/dashboard', function () {
     return view('pages.dashboard');
 });
