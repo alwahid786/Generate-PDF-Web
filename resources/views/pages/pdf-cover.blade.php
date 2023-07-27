e25a41df534506f2f0525f2d25757112dd6d715a
Jay <volcasoft@gmail.com>
2023-07-27 23:05:41 +0500

    Update pdf-cover.blade.php

--- resources/views/pages/pdf-cover.blade.php
+++ resources/views/pages/pdf-cover.blade.php
@@ -20,7 +20,7 @@
                 <h1>PDF Package Creator <span>14 Jul 2023</span></h1>
             </div>
         </div>
-        <iframe src="https://docs.google.com/viewer?url=http://www.pdf995.com/samples/pdf.pdf&embedded=true">
+        <iframe src="https://kodextech.com/Submittal_pkg.pdf">
         </iframe>
     </div>
 </main>
