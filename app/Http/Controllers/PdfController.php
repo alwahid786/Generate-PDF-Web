b0d041edf78674411644f73a67d49eab280ce4a4
Wahid Ahmad <wahid.a@kodextech.com>
2023-08-04 11:50:02 +0500

    update

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -9,6 +9,8 @@ use Spatie\PdfToImage\Pdf;
 use Spatie\PdfToImage\Exceptions\PdfDoesNotExist;
 use Exception;
 use Imagick;
+use Symfony\Component\Process\Process;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 
 class PdfController extends Controller
 {
@@ -24,22 +26,47 @@ class PdfController extends Controller
     {
         $fixtures = $request->fixtures;
         foreach ($fixtures as $fixture) {
-            $uploadedFile  = $fixture['pdfFile'];
+            $uploadedFile = $fixture['pdfFile'];
             if ($uploadedFile && $uploadedFile->isValid()) {
                 try {
                     $name = time() . $uploadedFile->getClientOriginalName();
                     $path = public_path('/files');
                     if (!is_dir($path)) {
-                        mkdir($path, 777, true);
+                        mkdir($path, 0777, true);
                     }
                     $uploadedFile->move($path, $name);
-                    $file = public_path('/files') . '/' .  $name;
-                    $image = new Imagick();
-                    $image->pingImage($file);
-                    // echo $image->getNumberImages();
+                    $file = $path . '/' . $name;
 
-                    dd($image->getNumberImages());
-                } catch (PdfDoesNotExist $e) {
+                    // Generate the output image path and filename
+                    $outputImagePath = $path . '/' . pathinfo($name, PATHINFO_FILENAME);
+
+                    // Update the command to use pdftoppm
+                    // $pageNumber = 1; // Replace with the desired page number to convert
+                    // $command = "pdftoppm -singlefile -png -f $pageNumber -l $pageNumber \"$file\" \"$outputImagePath\"";
+
+                    // // Execute the shell command using shell_exec
+                    // shell_exec($command);
+                    // // The output image file will have .png extension, not .jpg
+                    $outputImageFile = $outputImagePath . '.png';
+                    // dd($outputImageFile);
+
+                    // // Check if the conversion was successful and the output image exists
+                    // if (file_exists($outputImageFile)) {
+                    //     // Do whatever you need to do with the converted image here
+                    //     // For example, you can save the path in the database or display it to the user.
+                    //     dd("PDF page $pageNumber converted to image: $outputImageFile");
+                    // } else {
+                    //     dump("Error: Image conversion failed.");
+                    // }
+                    // $image = new Imagick();
+                    // $image->pingImage($file);
+                    // dd($image->getNumberImages());
+
+                    $pdf = new Pdf($file);
+                    $pdf->setPage(1); // Optional: Set image quality (100 is highest)
+                    $pdf->saveImage($outputImageFile);
+                    dd('done');
+                } catch (FileNotFoundException $e) {
                     dump("Error: PDF does not exist or is not accessible.");
                 } catch (Exception $e) {
                     dump("Error processing PDF: " . $e->getMessage());
