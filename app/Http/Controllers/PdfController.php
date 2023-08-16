6b150757ebf9a7a7013320eccabe457f3e5ce6a2
mubeen <mubeen.kodex@gmail.com>
2023-08-16 19:10:51 +0500

    Update PdfController.php

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -52,16 +52,16 @@ class PdfController extends Controller
             $uploadedFile = $fixture['pdfFile'];
             // $uploadedImageFile = $fixture['imageFile'];
 
-            // if ($uploadedFile && $uploadedFile->isValid()) {
+            if ($uploadedFile && $uploadedFile->isValid()) {
 
                 $name = time() . $uploadedFile->getClientOriginalName();
                 $path = public_path('/files');
 
                 $uploadedFile->move($path, $name);
                 $filePath = $path . '/' . $name;
-            // } else {
-                // return response()->json(['status' => false, 'message' => 'Error: File is Invalid!']);
-            // }
+            } else {
+                return response()->json(['status' => false, 'message' => 'Error: File is Invalid!']);
+            }
 
             // image upload
             // if ($uploadedImageFile && $uploadedImageFile->isValid()) {
@@ -72,8 +72,8 @@ class PdfController extends Controller
             //     $uploadedImageFile->move($imagePath, $imageName);
             //     $imageFilePath = $imagePath . '/' . $imageName;
             // } else {
+            //     return response()->json(['status' => false, 'message' => 'Error: File is Invalid!']);
             // }
-            // return response()->json(['status' => false, 'message' => 'Error: File is Invalid!']);
 
 
             $fixtureData = new Fixtures();
