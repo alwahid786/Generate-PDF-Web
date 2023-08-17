a9f7f20d3bb47baf858fdb10369bfd4ecb9e70bb
mubeen <mubeen.kodex@gmail.com>
2023-08-17 18:54:54 +0500

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -18,16 +18,16 @@
 
         @if (\Session::has('error'))
         <div class="alert alert-danger">
-            <ul>
-                <li>{{ \Session::get('error') }}</li>
-            </ul>
+            <div>
+                <div>{{ \Session::get('error') }}</div>
+            </div>
         </div>
         @endif
         @if (\Session::has('success'))
             <div class="alert alert-success">
-                <ul>
-                    <li>{{ \Session::get('success') }}</li>
-                </ul>
+                <div>
+                    <div>{{ \Session::get('success') }}</div>
+                </div>
             </div>
         @endif
 
