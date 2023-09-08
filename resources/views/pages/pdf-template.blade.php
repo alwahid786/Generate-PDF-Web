126c6e9c9f2cc6a8a5dd9c5a61c2c1dd70c128f4
mubeen <mubeen.kodex@gmail.com>
2023-09-08 20:17:42 +0500

    Update pdf-template.blade.php

--- resources/views/pages/pdf-template.blade.php
+++ resources/views/pages/pdf-template.blade.php
@@ -38,11 +38,8 @@ width:816px;
                 $formattedDate = $date->format('d F Y');
                 ?>
                 <div>
-                    <h1 style="font-size: 2.8rem; margin: 0" class="projectTitle">
-                        {{ $pdf_path[0]['fixture']['project'] }}
-                    </h1>
-                    <h3 style="font-size: 17px;margin-top: 6px;margin-bottom: 4px;font-weight: bold;"
-                        class="projectTypeName">{{ $packageTypeName[0] }}</h3>
+                    <h1 style="font-size: 2.8rem; margin: 0" class="projectTitle">{{ $pdf_path[0]['fixture']['project'] }}</h1>
+                    <h3 style="font-size: 17px;margin-top: 6px;margin-bottom: 4px;font-weight: bold;" class="projectTypeName">{{ $packageTypeName[0] }}</h3>
                     <p style="font-size: 14px;padding-top: 12px;">{{ $formattedDate }}</p>
                 </div>
                 <div
